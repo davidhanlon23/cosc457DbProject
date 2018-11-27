06347d72ec66561baddd9e6dbbcca0ef56f2290c
davidhanlon23 <32404063+davidhanlon23@users.noreply.github.com>
2018-11-26 19:34:50 -0500

    David Testing

--- cosc457DbProject/tattoo_site/client/src/pages/request-form.jsx
+++ cosc457DbProject/tattoo_site/client/src/pages/request-form.jsx
@@ -6,7 +6,7 @@ const RequestForm = () => {
         <main>
             <section>
                 <div className="page-header"> 
-                    <h2>*JUDE WAS HERE*Reservation Form</h2>
+                    <h2>*DADDY WAS HERE*Reservation Form</h2>
                 </div>
                 <div className="row">
                     <div className="col-md-6">
